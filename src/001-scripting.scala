5389b5c8e0c94d5fc6f50c9cb34a159de2a05c7b
Rafael Avila <rafa.avim@gmail.com>
2016-04-14 18:24:13 -0500

    add first atom: scripting

---
+++ src/001-scripting.scala
@@ -0,0 +1,6 @@
+/*
+ * we run this Scala script from the shell as follow
+ * $ scala 001-scripting.scala
+ * the result will be the `Hello, Scala!` message in the shell
+ */
+println("Hello, Scala!")
