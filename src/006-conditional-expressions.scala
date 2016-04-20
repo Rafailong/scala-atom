36f755fcf2efc4b5b5a4c0bbd4343046153bec54
Rafael Avila <rafa.avim@gmail.com>
2016-04-19 22:54:15 -0500

    conditional expression, exercise 3

--- src/006-conditional-expressions.scala
+++ src/006-conditional-expressions.scala
@@ -31,6 +31,7 @@ println(x)
 */
 val a = 1
 val b = 5
+val c = 5
 if(a < b) {
   println("a is less than b")
 }
@@ -54,4 +55,18 @@ if(b < 2) {
 }
 else {
   println("b is not less than 2")
+}
+
+/*
+3. Set the value c to 5. Modify the first exercise, above, to check if
+  a < c. Then, check if b < c (where ‘<’ is the less-than operator).
+  Print the results.
+*/
+if(a < c) {
+  println("a is less than c")
+  if(b < c) { println("b is less than c") }
+  else { println("b is not less than c") }
+}
+else {
+  println("a is not less than b")
 }
