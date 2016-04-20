83e11ec547e46b47fa16260a2e5d61896459f383
Rafael Avila <rafa.avim@gmail.com>
2016-04-19 22:43:21 -0500

    we can deal with both true and false path using the if-else stament.

--- src/006-conditional-expressions.scala
+++ src/006-conditional-expressions.scala
@@ -7,3 +7,10 @@ val r:Boolean = { 1 > 2 }
 if(!r) {
   println("It's false!")
 }
+
+// the `else` stament
+if(2 > 4) {
+  println("2 is greater than 4")
+} else {
+  println("4 is greater than 2")
+}
