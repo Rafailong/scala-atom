f43d6cd34993a4b942aca70395a942616d3de61c
Rafael Avila <rafa.avim@gmail.com>
2016-04-20 21:59:32 -0500

    compound expressions - intro

---
+++ src/008-compound-expressions/001-compound-expressions.scala
@@ -0,0 +1,14 @@
+// in Scala everything is an expression
+// there are two kinds of expressions
+// simple and compound expressions
+
+val r = 1 + 2 // simple expression
+println("result of a simple expression:" + r)
+
+// compound expression
+val rCompound = {
+  val a = 1
+  val b = 5
+  a + b
+}
+println("result of a compound expression: " + rCompound)
