35d9620385ca4daf29296447cb36c1b7ed7dcb7a
Rafael Avila <rafa.avim@gmail.com>
2016-04-25 22:38:06 -0500

    add first summary atom

---
+++ src/010-summary-1/exercises.scala
@@ -0,0 +1,9 @@
+//Work exercises 1-8 in the REPL
+
+// 1. Store and print an Int value.
+
+val one:Int = 1
+
+// 2. Tryt o change the value. What happened?
+// this will throw a reassignment error
+// one = 2
