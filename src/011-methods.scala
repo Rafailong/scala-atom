25de158acafff81db96c4025b79c1ca51319d885
Rafael Avila <rafa.avim@gmail.com>
2016-04-26 22:44:30 -0500

    exercise 1, getSquare

--- src/011-methods.scala
+++ src/011-methods.scala
@@ -13,11 +13,28 @@ def multiplyByTwo(number:Int):Int = {
 }
 println("10 multiply by two is ", multiplyByTwo(10))
 
+// the tests methods do not return a value
 def test(number:Int, expected:Int):Unit = {
   val result = multiplyByTwo(number)
   assert(result == expected, "Expected " + expected + " Got " + result)
   println("result", result)
 }
 test(2, 4)
-// test(2, 5) this throws and exception
 // java.lang.AssertionError: assertion failed: Expected 5 Got 4
+// test(2, 5) this throws and exception
+
+
+/**
+* EXERCISES
+*
+* 1. Create a method getSquare that takes and Int argument and returns its square.
+*/
+def getSquare(x:Int):Int = {
+  x * x
+}
+val a = getSquare(3)
+assert(a == 9, "Expected " + 9 + " Got " + a)
+val b = getSquare(6)
+assert(b == 36, "Expected " + 36 + " Got " + b)
+val c = getSquare(5)
+assert(c == 25, "Expected " + 25 + " Got " + c)
