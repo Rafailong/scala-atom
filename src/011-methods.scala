efbde2957e1c525f6b132cd9695b8f0b2c9c5e27
Rafael Avila <rafa.avim@gmail.com>
2016-04-26 22:37:25 -0500

    using assert method to test our methods

--- src/011-methods.scala
+++ src/011-methods.scala
@@ -8,7 +8,16 @@
 * A method body is an expression!
 */
 
-def multiplyByTwo (number:Int):Int = {
+def multiplyByTwo(number:Int):Int = {
   number * 2
 }
 println("10 multiply by two is ", multiplyByTwo(10))
+
+def test(number:Int, expected:Int):Unit = {
+  val result = multiplyByTwo(number)
+  assert(result == expected, "Expected " + expected + " Got " + result)
+  println("result", result)
+}
+test(2, 4)
+// test(2, 5) this throws and exception
+// java.lang.AssertionError: assertion failed: Expected 5 Got 4
