5f8d5c4e45dea4b5e7fe8be0c3afde9d77db281c
Rafael Avila <rafa.avim@gmail.com>
2016-04-26 22:26:02 -0500

    add first method example, multiply a given by tow

--- src/011-methods.scala
+++ src/011-methods.scala
@@ -4,4 +4,11 @@
 * def methodName(arg1:Type, arg2:Type, ...):ReturnType = {
 *   lines of code
 * }
+*
+* A method body is an expression!
 */
+
+def multiplyByTwo (number:Int):Int = {
+  number * 2
+}
+println("10 multiply by two is ", multiplyByTwo(10))
