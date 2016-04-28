40ed0051154988b59a8c4ca19cca7f6e9442b166
Rafael Avila <rafa.avim@gmail.com>
2016-04-27 21:35:09 -0500

    add first class and object exercise

--- src/012-classes-and-objects.scala
+++ src/012-classes-and-objects.scala
@@ -8,3 +8,19 @@ println(range1)
 val range2 = 0 until 10
 val range3 = range2.start until range2.end by range2.step + 1
 println(range2, range3)
+
+/**
+* EXERCISES
+*
+* 1. Create a Range object and print the step value. Create a second Range object with a step
+*   value of 2 and then print the step value. What's different?
+*   R. The difference is the number of jump that will take the range in its value.
+*/
+val range4 = Range(0, 6)
+println("range4 step value", range4.step)
+
+val range5 = Range(0, 10, 2) // the step is the last params
+println("range5 step value", range5.step)
+
+val testRange = range4.start until range4.end by range5.step
+assert(testRange.length == 3)
