094f45eab997e3d2cea36f8a3586e56c8aea0199
Rafael Avila <rafa.avim@gmail.com>
2016-04-28 22:33:32 -0500

    add first example of creating classes

---
+++ src/014-creating-classes/animals.scala
@@ -0,0 +1,16 @@
+/**
+* We use the keyword `class` to define type in Scala.
+*/
+
+class Giraffe
+class Bear
+class Hippo
+
+val g1 = new Giraffe
+val b = new Bear
+val h = new Hippo
+val h2 = new Hippo
+
+println(g1)
+println(b)
+println(h, h2)
