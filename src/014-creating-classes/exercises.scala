ec61059405e555cfdfe2c8f8bbb12dfbc7a871cf
Rafael Avila <rafa.avim@gmail.com>
2016-04-28 22:44:33 -0500

    add second exercise of 014 atom

--- src/014-creating-classes/exercises.scala
+++ src/014-creating-classes/exercises.scala
@@ -15,3 +15,15 @@ val m = new Monkey
 val g = new Giraffe
 
 println(h, l, t, m, g)
+
+/**
+* 2. Create a second instance of Lion and two more Giraffe.
+*   Print those objects.
+*   Those new objects differ  from the above in that they are new and
+*   stored in a diff momery. (Check the ugly number when printed)
+*/
+
+val l2 = new Lion
+val g2 = new Giraffe
+val g3 = new Giraffe
+println(l2, g2, g3)
