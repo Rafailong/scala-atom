1dc4d5206220b015fcfa05103cd06e89c38ea258
Rafael Avila <rafa.avim@gmail.com>
2016-04-28 22:41:18 -0500

    first exercise creating classes/types

---
+++ src/014-creating-classes/exercises.scala
@@ -0,0 +1,17 @@
+/**
+* 1. Create classes for Hippo, Lion, Tiger, Monkey, and Giraffe, then create an
+*   instance of each one of those classes.
+*/
+class Hippo
+class Lion
+class Tiger
+class Monkey
+class Giraffe
+
+val h = new Hippo
+val l = new Lion
+val t = new Tiger
+val m = new Monkey
+val g = new Giraffe
+
+println(h, l, t, m, g)
