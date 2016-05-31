db1d379e4f1bf68d76b6dc8384999dc52aa30e48
Rafael Avila <rafa.avim@gmail.com>
2016-05-30 22:59:23 -0500

    017 exercise 4 completed

--- src/017-testing/Exercises.scala
+++ src/017-testing/Exercises.scala
@@ -15,3 +15,17 @@ myVal1 is myValue3 // this will match
 val myValue5 = "10"
 
 myVal1 is myValue5 // this will match
+
+def squareArea(x:Double):Double = { x * x }
+squareArea(5) is 25
+squareArea(1.5) is 2.25
+
+def rectangleArea(x:Double, y:Double):Double = { x * y }
+rectangleArea(1, 2) is 2
+rectangleArea(1.5, 2) is 3
+
+def trapezoidArea(a:Double, b:Double, h:Double) = {
+  (a + b)/2 * h
+}
+trapezoidArea(2, 3, 1) is 2.5
+trapezoidArea(1, 10, 5) is 27.5
