dbeec9d432a581a6fbd6a7999e7eabd4680e4cdb
Rafael Avila <rafa.avim@gmail.com>
2016-05-30 23:06:07 -0500

    017 exercise 5 completed

--- src/017-testing/Exercises.scala
+++ src/017-testing/Exercises.scala
@@ -29,3 +29,11 @@ def trapezoidArea(a:Double, b:Double, h:Double) = {
 }
 trapezoidArea(2, 3, 1) is 2.5
 trapezoidArea(1, 10, 5) is 27.5
+
+squareArea(1) is 1
+squareArea(2) is 4
+squareArea(5) is 25
+rectangleArea(2, 2) is 4
+rectangleArea(5, 4) is 20
+trapezoidArea(2, 2, 4) is 8
+trapezoidArea(3, 4, 1) is 3.5
