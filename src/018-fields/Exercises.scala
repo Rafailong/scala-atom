f6c3e1087e52458ab2497ba489bbbacab5da8e4a
Rafael Avila <rafa.avim@gmail.com>
2016-05-31 15:01:41 -0500

    018 exercise 3 completed, we can get/set de percentFull, that really bad :"

--- src/018-fields/Exercises.scala
+++ src/018-fields/Exercises.scala
@@ -2,17 +2,22 @@ import com.atomicscala.AtomicTest._
 import me.rafaavila._
 
 // exercise 1, we do not need more code to sitisfy this test
-println("Cup2")
+println("ex 1")
 val cup2 = new Cup2
 cup2.add(45) is 45
 cup2.add(-15) is 30
 cup2.add(-50) is -20
 
 // exercise 2
-println("Cup3")
+println("ex 2")
 val cup3 = new Cup3
 cup3.add(45) is 45
 cup3.add(-55) is 0
 cup3.add(10) is 10
 cup3.add(-9) is 1
 cup3.add(-2) is 0
+
+// exercise 3
+println("ex 3")
+cup3.percentFull = 100
+cup3.percentFull is 100
