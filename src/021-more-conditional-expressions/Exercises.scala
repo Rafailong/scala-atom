188a6e489fb57b040a0f93489fda5f5d84883a6a
Rafael Avila <rafa.avim@gmail.com>
2016-06-02 18:19:39 -0500

    021 exercise 4

--- src/021-more-conditional-expressions/Exercises.scala
+++ src/021-more-conditional-expressions/Exercises.scala
@@ -21,3 +21,18 @@ def isPalindrome2(s:String):Boolean = {
 isPalindrome2("Mom") is true
 isPalindrome2("Dad") is true
 isPalindrome2("sTrEeT") is false
+
+// 4
+def stripSpecialChars(s:String):String = {
+  var x = ""
+  s.foreach(c => {
+    val asInt = c.toInt
+    // letras
+    if (asInt >= 65 && asInt <= 122) x += c
+    else if (asInt >= 48 && asInt <= 57) x += c
+  })
+  x
+}
+val isPalindromeComposed = isPalindrome2 _ compose stripSpecialChars _
+isPalindromeComposed("Madam I'm adam") is true
+isPalindromeComposed("trees") is false
