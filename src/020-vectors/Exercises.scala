b8275f89fdb8fd10482a8ec43044a0f697acfba5
Rafael Avila <rafa.avim@gmail.com>
2016-06-02 12:07:03 -0500

    020 add exercise 3

---
+++ src/020-vectors/Exercises.scala
@@ -0,0 +1,11 @@
+import com.atomicscala.AtomicTest._
+
+
+// 3
+var setence = ""
+val v1 = Vector("The", "dog", "visited", "the", "firehouse!")
+for(s <- v1) {
+  println(s)
+  setence += s + " "
+}
+setence is "The dog visited the firehouse! "
