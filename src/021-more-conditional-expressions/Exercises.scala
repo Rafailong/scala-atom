0bf51249c6184662f67e94613faa999138c05754
Rafael Avila <rafa.avim@gmail.com>
2016-06-02 17:48:55 -0500

    021 exercises 2 and 3

--- src/021-more-conditional-expressions/Exercises.scala
+++ src/021-more-conditional-expressions/Exercises.scala
@@ -6,3 +6,18 @@ val vector2 = Vector(1, 2, 1, 2, 1) // intercalados
 
 vector1 is vector1.reverse
 vector2 is vector2.reverse
+
+// 2
+def isPalindrome(s:String):Boolean = s.reverse == s
+isPalindrome("mom") is true
+isPalindrome("dad") is true
+isPalindrome("street") is false
+
+// 3
+def isPalindrome2(s:String):Boolean = {
+  val sLower = s.toLowerCase
+  sLower.reverse == sLower
+}
+isPalindrome2("Mom") is true
+isPalindrome2("Dad") is true
+isPalindrome2("sTrEeT") is false
