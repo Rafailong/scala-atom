6927555f2acde0b4e596f6b57e73aa1c837ad1af
Rafael Avila <rafa.avim@gmail.com>
2016-06-05 20:43:54 -0500

    023 exercise 3

--- src/023-pattern-matchin/Exercises.scala
+++ src/023-pattern-matchin/Exercises.scala
@@ -24,3 +24,18 @@ val v2 = Vector(3, 4)
 
 oneOrTheOther(v == v.reverse) is "true"
 oneOrTheOther(v2 == v2.reverse) is "false"
+
+// 3
+def checkTruth(a:Boolean, b:Boolean):String = {
+  (a, b) match {
+    case (true, true) => "Both are true"
+    case (true, false) => "First is true, second is false"
+    case (false, true) => "First is false, second is true"
+    case _ => "Both are false"
+  }
+}
+
+checkTruth(true || false, true) is "Both are true"
+checkTruth(1 > 0 && -1 < 0, 1 == 2) is "First is true, second is false"
+checkTruth(1 >= 2, 1>= 1) is "First is false, second is true"
+checkTruth(true && false, false && true) is "Both are false"
