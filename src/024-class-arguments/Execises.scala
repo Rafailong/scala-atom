eb8506ffa91728dd8cd8f9ee23d7ab8dcde89e48
Rafael Avila <rafa.avim@gmail.com>
2016-08-02 16:08:21 -0500

    024 exercise 3

--- src/024-class-arguments/Execises.scala
+++ src/024-class-arguments/Execises.scala
@@ -1,10 +1,8 @@
 import com.atomicscala.AtomicTest._
 
-// 2
+// 3
 class Family(dad:String, mom:String, children:String*) {
   def familySize():Int = { children.length + 2 }
 }
-val family1 = new Family("Mom", "Dad", "Sally", "Dick")
-family1.familySize() is 4
-val family2 = new Family("Mom", "Dad", "Harry")
-family2.familySize() is 3
+val family1 = new Family("Mom", "Dad")
+family1.familySize() is 2
