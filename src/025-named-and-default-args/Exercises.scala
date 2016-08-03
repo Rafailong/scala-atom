b2a73e0661a4fa3edd19d05bbaae568f5a1aa144
Rafael Avila <rafa.avim@gmail.com>
2016-08-03 15:31:11 -0500

    025 exercise 4

--- src/025-named-and-default-args/Exercises.scala
+++ src/025-named-and-default-args/Exercises.scala
@@ -1,8 +1,8 @@
 import com.atomicscala.AtomicTest._
 
-//2
+// 4
 class Planet(val name:String, val description:String, val moons:Int = 1) {
   val hasMoon = moons > 0
 }
-val p = new Planet(name = "Mercury", description = "small and hot planet", moons = 0)
-p.hasMoon is false
+val p = new Planet(moons = 2, name = "Mercury", description = "small and hot planet")
+p.hasMoon is true
