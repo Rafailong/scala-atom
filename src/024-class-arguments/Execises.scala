08a98d6c1b647bc05e5ac268fcd8685f8363637d
Rafael Avila <rafa.avim@gmail.com>
2016-08-03 14:32:36 -0500

    024 exercise 9

--- src/024-class-arguments/Execises.scala
+++ src/024-class-arguments/Execises.scala
@@ -1,18 +1,14 @@
 import com.atomicscala.AtomicTest._
 
-// 8
-class Cup2(var percentFull:Int) {
-  val max = 100
-  def add (increments:Int*):Int = {
-    for(i <- increments) {
-      percentFull += i
-      if(percentFull > 100) percentFull = 100
-      else if(percentFull < 0) percentFull = 0
-    }
-
-    percentFull
+// 9
+def squareThem(args:Int*):Int = {
+  var t = 0
+  for(n <- args) {
+    t += n * n
   }
+  t
 }
-val cup2 = new Cup2(0)
-cup2.add(20, 30, 50, 20, 10, -10, -40, 10, 50) is 100
-cup2.add(10, 10, -10, 10, 90, 70, -70) is 30
+
+squareThem(2) is 4
+squareThem(2, 4) is 20
+squareThem(1, 2, 4) is 21
