324416a5e1cb6e6211b8166e5ca16aae6ff13c92
Rafael Avila <rafa.avim@gmail.com>
2016-08-03 15:43:35 -0500

    025 exercise 6

--- src/025-named-and-default-args/Exercises.scala
+++ src/025-named-and-default-args/Exercises.scala
@@ -1,8 +1,21 @@
 import com.atomicscala.AtomicTest._
 
-// 4
-class Planet(val name:String, val description:String, val moons:Int = 1) {
-  val hasMoon = moons > 0
+// 6
+class Item(name:String, price:Double) {
+  def cost(grocery:Boolean = false, medication:Boolean = false, taxRate:Double = 0.10):Double = {
+    if (grocery || medication) {
+      price
+    } else {
+      (price * taxRate) + price
+    }
+  }
 }
-val p = new Planet(moons = 2, name = "Mercury", description = "small and hot planet")
-p.hasMoon is true
+
+val flour = new Item(name="flour", 4)
+flour.cost(grocery=true) is 4
+
+val sunscreen = new Item(name="sunscreen", 3)
+sunscreen.cost() is 3.3
+
+val tv = new Item(name="television", 500)
+tv.cost(taxRate=0.06) is 530
