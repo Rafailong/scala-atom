1c156e6569410ead5a65459e1b012b4989da5a84
Rafael Avila <rafa.avim@gmail.com>
2016-08-03 15:26:06 -0500

    025 exercise 2

--- src/025-named-and-default-args/Exercises.scala
+++ src/025-named-and-default-args/Exercises.scala
@@ -1,7 +1,7 @@
 import com.atomicscala.AtomicTest._
 
 // 1
-class SimpleTime(val hours:Int, val minutes:Int)
-val t = new SimpleTime(hours=5, minutes=30)
-t.hours is 5
-t.minutes is 30
+class SimpleTime(val hours:Int, val minutes:Int = 0)
+val t = new SimpleTime(hours=10)
+t.hours is 10
+t.minutes is 0
