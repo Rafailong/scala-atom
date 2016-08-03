00becf4f40e5a2d5c6968b1dc7e449587ca6b0f3
Rafael Avila <rafa.avim@gmail.com>
2016-08-03 14:29:04 -0500

    024 exercise 8

--- src/024-class-arguments/Execises.scala
+++ src/024-class-arguments/Execises.scala
@@ -1,18 +1,18 @@
 import com.atomicscala.AtomicTest._
 
-// 6
+// 8
 class Cup2(var percentFull:Int) {
   val max = 100
-  def add (increment:Int):Int = {
-    percentFull += increment
-    if (percentFull > max) {
-      percentFull = max
+  def add (increments:Int*):Int = {
+    for(i <- increments) {
+      percentFull += i
+      if(percentFull > 100) percentFull = 100
+      else if(percentFull < 0) percentFull = 0
     }
 
     percentFull
   }
 }
 val cup2 = new Cup2(0)
-cup2.add(45) is 45
-cup2.add(-15) is 30
-cup2.add(-50) is -20
+cup2.add(20, 30, 50, 20, 10, -10, -40, 10, 50) is 100
+cup2.add(10, 10, -10, 10, 90, 70, -70) is 30
