b2d9df0494ba964dabf8f6a35af054d38fde72b4
Rafael Avila <rafa.avim@gmail.com>
2016-08-03 15:29:05 -0500

    025 exercise 2

--- src/025-named-and-default-args/Exercises.scala
+++ src/025-named-and-default-args/Exercises.scala
@@ -1,7 +1,8 @@
 import com.atomicscala.AtomicTest._
 
-// 1
-class SimpleTime(val hours:Int, val minutes:Int = 0)
-val t = new SimpleTime(hours=10)
-t.hours is 10
-t.minutes is 0
+//2
+class Planet(val name:String, val description:String, val moons:Int = 1) {
+  val hasMoon = moons > 0
+}
+val p = new Planet(name = "Mercury", description = "small and hot planet", moons = 0)
+p.hasMoon is false
