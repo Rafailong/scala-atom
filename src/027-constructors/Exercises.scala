33bbd7e9315798ed019c0461791a035757038523
Rafael Avila <rafa.avim@gmail.com>
2016-08-04 08:22:43 -0500

    027 exercise 3

--- src/027-constructors/Exercises.scala
+++ src/027-constructors/Exercises.scala
@@ -1,7 +1,7 @@
 import com.atomicscala.AtomicTest._
 
-// 2
-class Tea(name:String = "Earl Grey", milk:Boolean = false, sugar:Boolean = false, decaffeinated:Boolean = false) {
+// 3
+class Tea(val name:String = "Earl Grey", val milk:Boolean = false, val sugar:Boolean = false, val decaffeinated:Boolean = false) {
   private def describeTea():String = {
     var desc = name
     if(milk) desc += " + milk"
@@ -24,15 +24,19 @@ class Tea(name:String = "Earl Grey", milk:Boolean = false, sugar:Boolean = false
 val t1 = new Tea
 t1.describe is "Earl Grey"
 t1.calories is 0
+t1.name is "Earl Grey"
 
 val t2 = new Tea(decaffeinated = true, name = "Lemon Zinger")
 t2.describe is "Lemon Zinger decaffeinated"
 t2.calories is 0
+t2.decaffeinated is true
 
 val sweetGreen = new Tea(name = "Jasmin Green", sugar = true)
 sweetGreen.describe is "Jasmin Green + sugar"
 sweetGreen.calories is 16
+sweetGreen.sugar is true
 
 val teaLatte = new Tea(sugar = true, milk = true)
 teaLatte.describe is "Earl Grey + milk + sugar"
 teaLatte.calories is 116
+teaLatte.milk is true
