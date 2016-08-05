f3dcaf1e9f00ffdf275585de99573b289920536d
Rafael Avila <rafa.avim@gmail.com>
2016-08-05 10:32:36 -0500

    028 exercise 4

--- src/028-auxiliary-ctors/Exercises.scala
+++ src/028-auxiliary-ctors/Exercises.scala
@@ -27,3 +27,15 @@ clothesWasher2a.capacity is 20.0
 val clothesWasher2b = new ClothesWasher(10.0)
 clothesWasher2b.modelName is "modelName"
 clothesWasher2b.capacity is 10.0
+
+// 4
+class ClothesWasher3(val modelName:String, val capacity:Double) {
+  def this(modelName:String) { this(modelName, 20.0) }
+  def this(capacity:Double) { this("modelName", capacity) }
+
+  def wash():String = "Simple wash"
+  def wash(bleach:Int, fabricSoftener:Int):String = s"Wash used $bleach bleach and $fabricSoftener fabric softener"
+}
+val washer = new ClothesWasher3("LG 100", 3.6)
+washer.wash(2, 1) is "Wash used 2 bleach and 1 fabric softener"
+washer.wash() is "Simple wash"
