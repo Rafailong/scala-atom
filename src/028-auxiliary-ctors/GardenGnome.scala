a30a8d31e7700bbfd1c5555569a9cfc7bde04419
Rafael Avila <rafa.avim@gmail.com>
2016-08-05 10:05:21 -0500

    028 aux ctors

---
+++ src/028-auxiliary-ctors/GardenGnome.scala
@@ -0,0 +1,20 @@
+import com.atomicscala.AtomicTest._
+
+class GardenGnome(val heigth:Double, val weight:Double, val happy:Boolean){
+  println("Inside primary ctor")
+  return
+  var painted:Boolean = true
+
+  // notice there is no equal to sign
+  def this(heigth:Double) { this(heigth, 100.0, true) }
+
+  def this(name:String) { this(15.0) }
+
+  def magic(level:Int):String = { "Poof! " +level }
+
+  def show():String = { s"$heigth $weight $happy $painted" }
+}
+
+(new GardenGnome(20.0, 110.0, false)).show() is "20.0 110.0 false true"
+
+(new GardenGnome("Bob")).show() is "15.0 100.0 true true"
