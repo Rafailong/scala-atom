0c4178a589ebdce2f56174a5a15a2f06ae95b8df
Rafael Avila <rafa.avim@gmail.com>
2016-08-12 14:50:31 -0500

    029 class exercises - 3

--- src/029-class-exercises/Info.scala
+++ src/029-class-exercises/Info.scala
@@ -1,9 +1,11 @@
 import com.atomicscala.AtomicTest._
 
-class Info(val name:String, var description:String)
+class Info(var name:String, var description:String)
 
 val info = new Info("stuff", "Something")
 info.name is "stuff"
+info.name = "this is the new name"
+info.name is "this is the new name"
 info.description is "Something"
 info.description = "Something else"
 info.description is "Something else"
