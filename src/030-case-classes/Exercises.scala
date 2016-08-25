243608e5f5e65eb179e52f49c77d36162572cf04
Rafael Avila <rafa.avim@gmail.com>
2016-08-24 21:35:55 -0500

    030 case classes, exercise 3

--- src/030-case-classes/Exercises.scala
+++ src/030-case-classes/Exercises.scala
@@ -5,3 +5,12 @@ val p = Person("Jane", "Smile", "jane@smile.com")
 p.first is "Jane"
 p.last is "Smile"
 p.email is "jane@smile.com"
+
+val people = Vector(
+  Person("Jane", "Smile", "jane@smile.com"),
+  Person("Ron", "House", "ron@house.com"),
+  Person("Sally", "Dove", "sally@dove.com")
+)
+people(0) is "Person(Jane,Smile,jane@smile.com)"
+people(1) is "Person(Ron,House,ron@house.com)"
+people(2) is "Person(Sally,Dove,sally@dove.com)"
