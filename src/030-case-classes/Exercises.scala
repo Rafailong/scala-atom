1bc9d7d6fa211fbcc493b3b4f5706c837f66f712
Rafael Avila <rafa.avim@gmail.com>
2016-08-24 21:42:31 -0500

    030 case classes, exercise 4

--- src/030-case-classes/Exercises.scala
+++ src/030-case-classes/Exercises.scala
@@ -24,3 +24,12 @@ val dogs = Vector(
 dogs(0) is "Dog(Fido,Golden Lab)"
 dogs(1) is "Dog(Ruff,Alaskan Malamute)"
 dogs(2) is "Dog(Fifi,Miniature Poodle)"
+
+case class Dimension(var height:Int, var width:Int)
+val c = Dimension(5, 7)
+c.height is 5
+c.height = 10
+c.height is 10
+c.width is 7
+c.width = 19
+c.width is 19
