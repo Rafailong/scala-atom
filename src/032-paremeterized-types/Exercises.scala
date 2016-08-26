4b32f604b31198c0b7c34b4816d709151ec6fc3e
Rafael Avila <rafa.avim@gmail.com>
2016-08-25 23:14:13 -0500

    032 parameterized types exercises 3

--- src/032-paremeterized-types/Exercises.scala
+++ src/032-paremeterized-types/Exercises.scala
@@ -12,3 +12,10 @@ def explicitVectorToList(v:Vector[Double]):List[Double] =
 
 explicitVectorToList(Vector(10.0, 20.0)) is List(10.0, 20.0)
 explicitVectorToList(Vector(1, 2, 3)) is List(1.0, 2.0, 3.0)
+
+// 3
+def explicitVectorToSet(v:Vector[Double]):Set[Double] =
+  v.toSet
+
+explicitVectorToSet(Vector(10.0, 20.0, 10.0)) is Set(10.0, 20.0)
+explicitVectorToSet(Vector(1, 2, 3, 2, 3, 4)) is Set(1.0, 2.0, 3.0, 4.0)
