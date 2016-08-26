0b579190d9a174e72934b634acf233793bae553d
Rafael Avila <rafa.avim@gmail.com>
2016-08-25 23:31:55 -0500

    032 parameterized types exercises 4

--- src/032-paremeterized-types/Exercises.scala
+++ src/032-paremeterized-types/Exercises.scala
@@ -19,3 +19,20 @@ def explicitVectorToSet(v:Vector[Double]):Set[Double] =
 
 explicitVectorToSet(Vector(10.0, 20.0, 10.0)) is Set(10.0, 20.0)
 explicitVectorToSet(Vector(1, 2, 3, 2, 3, 4)) is Set(1.0, 2.0, 3.0, 4.0)
+
+// 4
+private def forecast(percent:Int):String = {
+  percent match {
+    case 100 => "Sunny"
+    case 80 => "Mostly Sunny"
+    case 50 => "Partly Sunny"
+    case 20 => "Mostly Cloudy"
+    case 0 => "Cloudy"
+    case _ => "Unknown"
+  }
+}
+def historicalData(v:Vector[Int]) = {
+  v.map(forecast(_)).groupBy(w => w).mapValues(_.size)
+}
+val v = Vector(100, 80, 20, 100, 20)
+historicalData(v) is "Map(Sunny -> 2, Mostly Cloudy -> 2, Mostly Sunny -> 1)"
