41af5576caad6e632255c6015f535349006a4a43
Rafael Avila <rafa.avim@gmail.com>
2016-08-25 23:12:03 -0500

    032 parameterized types exercises 2

--- src/032-paremeterized-types/Exercises.scala
+++ src/032-paremeterized-types/Exercises.scala
@@ -5,3 +5,10 @@ def explicit(c1:Double, c2:Double, c3:Double):Vector[Double] =
   Vector(c1, c2, c3)
 
 explicit(1.0, 2.0, 3.0) is Vector(1.0, 2.0, 3.0)
+
+// 2
+def explicitVectorToList(v:Vector[Double]):List[Double] =
+  v.toList
+
+explicitVectorToList(Vector(10.0, 20.0)) is List(10.0, 20.0)
+explicitVectorToList(Vector(1, 2, 3)) is List(1.0, 2.0, 3.0)
