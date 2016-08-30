b2908b94668c9e1dc545f462dc1a216a1a1c1888
Rafael Avila <rafa.avim@gmail.com>
2016-08-29 22:09:48 -0500

    033 exercises 4

--- src/033-functions-objects/Exercises.scala
+++ src/033-functions-objects/Exercises.scala
@@ -14,3 +14,9 @@ s is "1,2,3,4,"
 // 3
 val dogYears = (y:Int) => y * 7
 dogYears(10) is 70
+
+// 4
+s = ""
+val v3 = Vector(1, 5, 7, 8)
+v3.foreach(n => s = s + s"${dogYears(n)} ")
+s is "7 35 49 56 "
