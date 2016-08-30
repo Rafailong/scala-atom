36aa1807fbaf823b391760fc711064d2ed8b6e40
Rafael Avila <rafa.avim@gmail.com>
2016-08-29 22:18:43 -0500

    033 exercises 7

--- src/033-functions-objects/Exercises.scala
+++ src/033-functions-objects/Exercises.scala
@@ -32,3 +32,9 @@ val between = (t:Int, l:Int, h:Int) => {
 }
 between(70, 80, 90) is false
 between(70, 60, 90) is true
+
+// 7
+s = ""
+val v7 = Vector(1, 2, 5, 3, 7)
+v7.foreach(n => s = s + s"${n * n} ")
+s is "1 4 25 9 49 "
