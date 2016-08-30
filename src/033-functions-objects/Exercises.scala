e752c2b5922237cce6970c76435fa4bfe9848e11
Rafael Avila <rafa.avim@gmail.com>
2016-08-29 22:05:16 -0500

    033 exercises 2

--- src/033-functions-objects/Exercises.scala
+++ src/033-functions-objects/Exercises.scala
@@ -3,5 +3,10 @@ import com.atomicscala.AtomicTest._
 // 1
 val v1 = Vector(1, 2, 3, 4)
 var s = ""
+v1.foreach(c => s = s + c)
+s is "1234"
+
+// 2
+s = ""
 v1.foreach(c => s = s + c + ",")
 s is "1,2,3,4,"
