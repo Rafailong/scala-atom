9c06a052cfd6b23f26711c781dc0ca49598bffc8
Rafael Avila <rafa.avim@gmail.com>
2016-08-30 22:12:22 -0500

    034 exercises 3

--- src/034-map-and-reduce/Exercises.scala
+++ src/034-map-and-reduce/Exercises.scala
@@ -9,3 +9,10 @@ vR is Vector(21, 32, 43, 54)
 var set = scala.collection.mutable.Set[Int]()
 v.foreach(n => set += (n * 11) + 10)
 set.toVector.sorted is Vector(21, 32, 43, 54)
+
+ // 3
+var set2 = scala.collection.mutable.Set[Int]()
+for (i <- v) {
+  set2 += (i * 11) + 10
+}
+set2.toVector.sorted is Vector(21, 32, 43, 54)
