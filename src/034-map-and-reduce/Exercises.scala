d5073b0ab807d0a16d32046574ddefac54b53d49
Rafael Avila <rafa.avim@gmail.com>
2016-08-30 22:09:39 -0500

    034 exercises 2

--- src/034-map-and-reduce/Exercises.scala
+++ src/034-map-and-reduce/Exercises.scala
@@ -4,3 +4,8 @@ import com.atomicscala.AtomicTest._
 val v = Vector(1, 2, 3, 4)
 val vR = v.map(n => (n * 11) + 10)
 vR is Vector(21, 32, 43, 54)
+
+// 2
+var set = scala.collection.mutable.Set[Int]()
+v.foreach(n => set += (n * 11) + 10)
+set.toVector.sorted is Vector(21, 32, 43, 54)
