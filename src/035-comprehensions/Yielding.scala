a05180d6890c9e5651a8c0794cc63d29398fcabb
Rafael Avila <rafa.avim@gmail.com>
2016-08-31 22:44:50 -0500

    035 comprehension assigning

--- src/035-comprehensions/Yielding.scala
+++ src/035-comprehensions/Yielding.scala
@@ -8,6 +8,15 @@ def yielding(v:Vector[Int]):Vector[Int] = {
   } yield n
   r
 }
-
 val v = Vector(1, 2, 3, 4, 5, 6, 7, 8, 10, 13, 14, 17)
 yielding(v) is Vector(1, 3, 5, 7)
+
+def yielding2(v:Vector[Int]):Vector[Int] = {
+  for {
+    n <- v
+    gt = n < 10
+    isOdd = n % 2 != 0
+    if(gt && isOdd)
+  } yield n
+}
+yielding2(v) is Vector(1, 3, 5, 7)
