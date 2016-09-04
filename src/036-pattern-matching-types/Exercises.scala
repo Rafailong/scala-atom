3cd0c1f99b3beb3f16744739fb065f0e42a1bbc3
Rafael Avila <rafa.avim@gmail.com>
2016-09-03 23:19:00 -0500

    036 exercises - 2

--- src/036-pattern-matching-types/Exercises.scala
+++ src/036-pattern-matching-types/Exercises.scala
@@ -13,3 +13,20 @@ def plus1(arg: Any): Any = {
 plus1(67) is 68
 plus1("car") is "cars"
 plus1(Person("Rafa")) is "Person(Rafa) + guest"
+
+def convertToSize(x: Any): Double = {
+  x match {
+    case s:String => s.size
+    case p:Person => 1d
+    case n:Int => n
+    case n:Float => n
+    case n:Double => n
+    case _ => 0d
+  }
+}
+convertToSize(45) is 45
+convertToSize("car") is 3
+convertToSize("truck") is 5
+convertToSize(Person("Rafa")) is 1
+convertToSize(45.6F) is 45.6F
+convertToSize(Vector(1, 2, 3)) is 0
