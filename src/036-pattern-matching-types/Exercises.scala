7f1d4b0ddca283acbbd593bcbc7f3c86fa885ffe
Rafael Avila <rafa.avim@gmail.com>
2016-09-03 23:22:26 -0500

    036 exercises - 3

--- src/036-pattern-matching-types/Exercises.scala
+++ src/036-pattern-matching-types/Exercises.scala
@@ -14,19 +14,18 @@ plus1(67) is 68
 plus1("car") is "cars"
 plus1(Person("Rafa")) is "Person(Rafa) + guest"
 
-def convertToSize(x: Any): Double = {
+def convertToSize(x: Any): Int = {
   x match {
     case s:String => s.size
-    case p:Person => 1d
+    case p:Person => 1
     case n:Int => n
-    case n:Float => n
-    case n:Double => n
-    case _ => 0d
+    case n:Float => scala.math.round(n)
+    case _ => 0
   }
 }
 convertToSize(45) is 45
 convertToSize("car") is 3
 convertToSize("truck") is 5
 convertToSize(Person("Rafa")) is 1
-convertToSize(45.6F) is 45.6F
+convertToSize(45.6F) is 46
 convertToSize(Vector(1, 2, 3)) is 0
