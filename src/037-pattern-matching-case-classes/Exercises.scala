27a810dcf91f742c6c677b88e30a66be02afb9ea
Rafael Avila <rafa.avim@gmail.com>
2016-09-04 21:02:33 -0500

    037 exercises 2

--- src/037-pattern-matching-case-classes/Exercises.scala
+++ src/037-pattern-matching-case-classes/Exercises.scala
@@ -19,7 +19,7 @@ def tranvel(t:Any):String = t match {
   case Train(travelers, line) => s"Train line $line $travelers"
   case Bus(travelers, seats) => s"Bus size $seats $travelers"
   case Plane(name, travelers) => s"Plane $name $travelers"
-  case Traveler => "Walkin along!"
+  case t:Traveler => s"${t.first} is walking"
   case x => s"$x is in limbo!"
 }
 
@@ -37,3 +37,4 @@ val trips = Vector(
 tranvel(trips(0)) is "Train line Reading Vector(Traveler(Rafa,Avila), Traveler(March,Avila))"
 tranvel(trips(1)) is "Bus size 100 Vector(Traveler(Rafa,Avila), Traveler(March,Avila))"
 tranvel(trips(2)) is "Plane B757 Vector(Traveler(Rafa,Avila), Traveler(March,Avila))"
+tranvel(travelers(0)) is "Rafa is walking"
