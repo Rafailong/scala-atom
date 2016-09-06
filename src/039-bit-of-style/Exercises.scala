47574848909ac2228f7ae39bb0cf3e263c26dec0
Rafael Avila <rafa.avim@gmail.com>
2016-09-05 22:22:00 -0500

    039 bit of style
    exercises 3 and 4

--- src/039-bit-of-style/Exercises.scala
+++ src/039-bit-of-style/Exercises.scala
@@ -1,10 +1,14 @@
 import com.atomicscala.AtomicTest._
 
-class Exclaim(var s:String) {
-  def parens() = s"$s!"
-  val noParens = s"$s!"
+class Exclaim3(var s:String, var count:Int = 0) {
+  def parens() = {
+    this.count += 1
+    s"$s!"
+  }
+  val noParens = s"$s!" // no need to remove, it wont change once assigned
 }
 
-val e = new Exclaim("hi")
+val e = new Exclaim3("hi")
 e.parens() is "hi!"
-e.noParens is "hi!"
+e.parens() is "hi!"
+e.count is 2
