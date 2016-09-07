b6ccab548298f5a9bf618bac198979170eb1bf12
Rafael Avila <rafa.avim@gmail.com>
2016-09-06 22:02:56 -0500

    040 defining operators
    exercise 1

---
+++ src/040-defining-operators/Exercises.scala
@@ -0,0 +1,26 @@
+import com.atomicscala.AtomicTest._
+
+class SimpleTime(val h:Int, val m:Int) {
+  def -(simpleTime:SimpleTime):SimpleTime = {
+    val thisMins = (this.h * 60) + this.m
+    val otherMins = (simpleTime.h * 60) + simpleTime.m
+
+    val subs = thisMins - otherMins
+    if (subs <= 0)
+      return new SimpleTime(0, 0)
+
+    val hrs:Int = subs/60
+    val mns = subs - (hrs * 60)
+    new SimpleTime(hrs, mns)
+  }
+}
+
+val t1 = new SimpleTime(10, 30)
+val t2 = new SimpleTime(9, 30)
+val st = t1 - t2
+st.h is 1
+st.m is 0
+
+val st2 = new SimpleTime(10, 30) - new SimpleTime(9, 45)
+st2.h is 0
+st2.m is 45
