3e0640962525beb440987f7e725e9a2cb4d48d47
Rafael Avila <rafa.avim@gmail.com>
2016-09-06 22:24:50 -0500

    041 automatic string conversion
    example 2

--- src/041-automatic-string-conversion/Examples.scala
+++ src/041-automatic-string-conversion/Examples.scala
@@ -3,3 +3,8 @@ import com.atomicscala.AtomicTest._
 case class Bicycle(riders:Int)
 val for2 = Bicycle(2)
 for2 is "Bicycle(2)"
+
+
+class Surrey(val adornment:String)
+val s = new Surrey("adornment")
+println(s) // fak :/
