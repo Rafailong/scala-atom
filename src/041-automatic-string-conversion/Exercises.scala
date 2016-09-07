562804c472ae7ca1495811d2d399d964bbf8d5fe
Rafael Avila <rafa.avim@gmail.com>
2016-09-06 22:33:03 -0500

    041 automatic string conversion
    exercise 2

--- src/041-automatic-string-conversion/Exercises.scala
+++ src/041-automatic-string-conversion/Exercises.scala
@@ -1,7 +1,19 @@
 import com.atomicscala.AtomicTest._
 
-case class Bicycle(riders:Int) {
-  override def toString(): String =  s"Bicycle built for $riders"
+case class Cycle(wheels:Int) {
+  override def toString(): String =  {
+    wheels match {
+      case 1 => "Unicycle"
+      case 2 => "Bicycle"
+      case _ => s"Cycle with $wheels wheels"
+    }
+  }
 }
-val for2 = Bicycle(2)
-for2 is "Bicycle built for 2"
+var uni = Cycle(1)
+uni is "Unicycle"
+
+val for2 = Cycle(2)
+for2 is "Bicycle"
+
+val five = Cycle(5)
+five is "Cycle with 5 wheels"
