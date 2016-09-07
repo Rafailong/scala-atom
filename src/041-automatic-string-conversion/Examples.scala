61d3df994bfa69d30c5c2f45d6a475a7701231a2
Rafael Avila <rafa.avim@gmail.com>
2016-09-06 22:27:04 -0500

    041 automatic string conversion
    exmaple 3. toString overriding

--- src/041-automatic-string-conversion/Examples.scala
+++ src/041-automatic-string-conversion/Examples.scala
@@ -8,3 +8,10 @@ for2 is "Bicycle(2)"
 class Surrey(val adornment:String)
 val s = new Surrey("adornment")
 println(s) // fak :/
+
+class Surrey2(val adornment:String) {
+  override def toString(): String =
+    s"Surrey2 with $adornment"
+}
+var s2 = new Surrey2("adornment")
+s2.toString is "Surrey2 with adornment"
