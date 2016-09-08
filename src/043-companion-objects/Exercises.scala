b64309c0e0b02e5f79e317663b370a03d09e9085
Rafael Avila <rafa.avim@gmail.com>
2016-09-07 22:39:48 -0500

    043 companion objects
    exercise 2

--- src/043-companion-objects/Exercises.scala
+++ src/043-companion-objects/Exercises.scala
@@ -2,8 +2,17 @@ import com.atomicscala.AtomicTest._
 
 class WalkActivity()
 object WalkActivity {
-  def start(name:String) { println("started!") }
+  private var log = ""
+  def start(name:String) {
+    log = s"[$name] Activiy started."
+    println(log)
+  }
+  def stop(name:String) {
+    log = s"[$name] Activiy stoped."
+    println(log)
+  }
 }
 
 // no need to init the WalkActivity object
 WalkActivity.start("Bob")
+WalkActivity.stop("Bob")
