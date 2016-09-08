c6a641afcd9883fc199a3fe91e5bcdfefa1b6359
Rafael Avila <rafa.avim@gmail.com>
2016-09-07 22:52:41 -0500

    043 companion objects
    exercise 4

--- src/043-companion-objects/Exercises.scala
+++ src/043-companion-objects/Exercises.scala
@@ -5,7 +5,14 @@ class WalkActivity() {
     WalkActivity.calories(lbs, mins, mph)
 }
 object WalkActivity {
-  val MET = 2.3
+  def MET(mph:Double) = mph match {
+    case x if(x < 1.7) => 2.3
+    case x if(x < 2.5) => 2.9
+    case x if(x < 3) => 3.3
+    case x if(x >= 3) => 3.3
+    case _ => 2.3
+  }
+
   private var log = ""
   def start(name:String) {
     log = s"[$name] Activiy started."
@@ -16,7 +23,7 @@ object WalkActivity {
     println(log)
   }
   def calories(lbs:Int, mins:Int, mph:Double=3):Long =
-    math.round((MET * 3.5 * lbs * 0.45)/200d * mins)
+    math.round((MET(mph) * 3.5 * lbs * 0.45)/200d * mins)
 }
 
 // no need to init the WalkActivity object
@@ -24,4 +31,7 @@ WalkActivity.start("Bob")
 WalkActivity.stop("Bob")
 
 val bob = new WalkActivity
-bob.calories(150, 30) is 82
+bob.calories(150, 30) is 117
+
+val fred = WalkActivity
+fred.calories(150, 30, 1.5) is 82
