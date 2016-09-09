45aafeee83f024e8ece0b38cae9ed571d4853d28
Rafael Avila <rafa.avim@gmail.com>
2016-09-08 22:21:57 -0500

    045 base class init
    exercise 3

--- src/045-base-class-initialization/Exercise.scala
+++ src/045-base-class-initialization/Exercise.scala
@@ -25,3 +25,17 @@ display(new BonoboB(100d, 12)) is "weight: 100.0 age: 12"
 display(new BonoboC(100d, 22)) is "weight: 100.0 age: 22"
 
 new Bonobo(100, 12).myWords is Vector("Roar")
+
+
+class House(val address:String, val state:String, val zip:String) {
+  def this(zip:String) = this("address?", "state?", zip)
+  def this(state:String, zip:String) = this("address?", state, zip)
+}
+
+class Home(address:String, state:String, zip:String, val name:String, val heart:Boolean =  true)
+  extends House(address, state, zip) {
+    override def toString(): String = "Where the heart is"
+}
+val h = new Home("address", "st", "cp", "mine")
+h.toString is "Where the heart is"
+h.heart is true
