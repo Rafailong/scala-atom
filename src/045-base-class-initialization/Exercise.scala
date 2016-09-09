7339c324a99879e2b4d85aa2705eb822fbbaa195
Rafael Avila <rafa.avim@gmail.com>
2016-09-08 22:32:04 -0500

    045 base class init
    exercise 4

--- src/045-base-class-initialization/Exercise.scala
+++ src/045-base-class-initialization/Exercise.scala
@@ -39,3 +39,31 @@ class Home(address:String, state:String, zip:String, val name:String, val heart:
 val h = new Home("address", "st", "cp", "mine")
 h.toString is "Where the heart is"
 h.heart is true
+
+class VacationHouse(
+  state:String,
+  zip:String,
+  val startMonth:Int,
+  val endMonth:Int)
+  extends House(state, zip) {
+    private def getMonthName(month:Int):String = month match {
+      case 1 => "January"
+      case 2 => "February"
+      case 3 => "March"
+      case 4 => "April"
+      case 5 => "May"
+      case 6 => "June"
+      case 7 => "July"
+      case 8 => "August"
+      case 9 => "September"
+      case 10 => "October"
+      case 11 => "November"
+      case 12 => "December"
+      case _ => "Dunno"
+    }
+
+    override def toString(): String =
+      s"Rented house is $state for months of ${getMonthName(startMonth)} through ${getMonthName(endMonth)}"
+}
+val v = new VacationHouse("MI", "49431", 6, 8)
+v is "Rented house is MI for months of June through August"
