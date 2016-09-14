02d6d470a4693c6079c7167222030fa9ab32ca6e
Rafael Avila <rafa.avim@gmail.com>
2016-09-13 22:33:54 -0500

    046 overring methods
    exercise 2

--- src/046-overriding-methods/Exercises.scala
+++ src/046-overriding-methods/Exercises.scala
@@ -5,3 +5,17 @@ import com.atomicscala.AtomicTest._
  * the method eat at GreatApe3 is defined with parens du to
  * is modifies the inner state of the object.
  */
+
+// 2
+class GreatApe(val weight:Double, val age:Int, val other:String = "") {
+  def myWords = Vector("Roar")
+}
+class Bonobo(weight:Double, age:Int)
+  extends GreatApe(weight, age) {
+
+  override def myWords = super.myWords :+ "Hello"
+}
+val ape = new GreatApe(112, 9)
+ape.myWords is Vector("Roar")
+val bonobo = new Bonobo(150, 14)
+bonobo.myWords is Vector("Roar", "Hello")
