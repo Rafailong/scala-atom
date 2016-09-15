098b5b70fee6337c174808de4bc12727eb37d905
Rafael Avila <rafa.avim@gmail.com>
2016-09-14 21:47:19 -0500

    047 enumerations
    exercise 2

--- src/047-enumerations/Exercises.scala
+++ src/047-enumerations/Exercises.scala
@@ -4,7 +4,20 @@ import com.atomicscala.AtomicTest._
 // 1
 object MonthName extends Enumeration {
   type MonthName = Value
-  val January, February = Value
+  val January = Value(1)
+  val February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December = Value
 }
 MonthName.February is "February"
-MonthName.February.id is 1
+MonthName.February.id is 2
+MonthName.December.id is 12
+MonthName.July.id is 7
